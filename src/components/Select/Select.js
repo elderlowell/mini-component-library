bb140faf5fbc985f8c6ea1088a505963815f6a85
Jonathan Lowell <elderlowell@gmail.com>
2024-04-29 11:42:10 -0600

    select component completed

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,69 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from '../../constants'
+import Icon from '../Icon'
+import { getDisplayedValue } from './Select.helpers'
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+  const displayedValue = getDisplayedValue(value, children)
 
+  const Wrapper = styled.div`
+    position: relative;
+    width: max-content;
+  `
+  const NativeSelect = styled.select`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    appearance: none;
+    opacity: 0;
+  `
+  const DisplayedSelect = styled.div`
+    background-color: ${COLORS.transparentGray15};
+    color: ${COLORS.gray700};
+    border-radius: 8px;
+    padding: 12px 16px;
+    padding-right: 52px;
+    font-size: ${16 / 16}rem;
+    font-weight: 400;
+    font-family: 'Roboto', sans-serif;
+    display: flex;
+    justify-content: space-between;
+
+    ${NativeSelect}:focus + & {
+      outline: 1px dotted #212121;
+      outline: 5px auto -webkit-focus-ring-color;
+    }
+    ${NativeSelect}:hover + & {
+      color: ${COLORS.black};
+    }
+  `
+  const IconWrapper = styled.div`
+    position: absolute;
+    right: 10px;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    width: var(--size);
+    height: var(--size);
+    pointer-events: none;
+  `
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
-};
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <DisplayedSelect>
+        {displayedValue}
+        <IconWrapper style={{ '--size': 24 + 'px' }}>
+          <Icon id="chevron-down" size={24} strokeWidth={2} />
+        </IconWrapper>
+      </DisplayedSelect>
+    </Wrapper>
+  )
+}
 
-export default Select;
+export default Select
